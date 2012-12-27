734120e44689e848fdb8e5d0dc0c9041d20b8d73
kathar <tehepero.kathar@gmail.com>
2012-12-28 02:07:15 +0900

    エラーコードを扱うクラスを追加

---
+++ KaLibCs/std/KaBasicError.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ka
+{
+	/// <summary>
+	/// 基本的なエラーコードを列挙型で定義
+	/// </summary>
+	public enum BasicErrorCode_e
+	{
+		no,
+		input,
+		file,
+		directory,
+
+		unknown
+	}
+
+	/// <summary>
+	/// 入力に関する詳細なエラーコード
+	/// </summary>
+	public enum InputErrorCode_e
+	{
+		no,
+
+		unknown
+	}
+
+	/// <summary>
+	/// ファイルに関する詳細なエラーコード
+	/// </summary>
+	public enum FileErrorCode_e
+	{
+		no,
+
+		unknown
+	}
+
+	/// <summary>
+	/// ディレクトリに関する詳細なエラーコード
+	/// </summary>
+	public enum DirectoryErrorCode_e
+	{
+		no,
+
+		unknown
+	}
+}
---
+++ KaLibCs/std/KaErrorBase.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ka
+{
+	/// <summary>
+	/// エラーコードの扱いを支援する基底クラス
+	/// </summary>
+	public class KaErrorBase
+	{
+
+	}
+}
