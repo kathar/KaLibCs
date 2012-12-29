10ba9017da2c571b6c0e879b2d1049d9d3a252f2
kathar <tehepero.kathar@gmail.com>
2012-12-29 16:17:48 +0900

    デスクトップと同期させるためにコミット

---
+++ KaLibCsForGame/std/MapChipData.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ka.Game
+{
+	public class MapChipData
+	{
+		public ChipType_e chipType { get; set; }
+
+	}
+}
---
+++ KaLibCsForGame/std/MapChipEnuｍs.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ka.Game
+{
+	public enum ChipType_e
+	{
+		solid,
+		solid_effect,
+		back,
+		back_effect,
+
+		empty
+	}
+}
